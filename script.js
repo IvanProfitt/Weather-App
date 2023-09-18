82965e3b240f598479ac51e3485a4436c7052454
Ivan Profitt <93939178+IvanProfitt@users.noreply.github.com>
2023-09-18 13:07:27 -0400

    Add background. Change Unit button functionality.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const changeUnits=document.getElementById("unitChange");
+let betterUnits=true;
+
 const cityName=document.getElementById("cityName");
 const cityTemp=document.getElementById("cityTemp");
 const todayImg=document.getElementById("todayImg");
@@ -27,7 +30,45 @@ function getDayOfWeek(epochTime) {
 }
 
 
-fetch('http://api.weatherapi.com/v1/forecast.json?key=68c77b33187c450e90e174348231609&q=47150&days=3&aqi=no&alerts=no')
+
+changeUnits.addEventListener("click", function(){
+    if(betterUnits===true){
+        fetch('http://api.weatherapi.com/v1/forecast.json?key=68c77b33187c450e90e174348231609&q=47150&days=3&aqi=no&alerts=no')
+    .then(function(response) {
+      return response.json();
+
+    })
+    .then(function(response){
+      cityTemp.innerHTML=response.current.temp_c;
+      cityName.innerHTML=response.location.name;
+      weatherCondition.innerHTML=response.current.condition.text;
+      todayImg.src="https:"+`${response.current.condition.icon}`;
+
+
+      forecastOneDate.innerHTML=getDayOfWeek(response.forecast.forecastday[0].date_epoch);
+      forecastOneMin.innerHTML=response.forecast.forecastday[0].day.maxtemp_c;
+      forecastOneMax.innerHTML=response.forecast.forecastday[0].day.mintemp_c;
+      imgOne.src="https:"+`${response.forecast.forecastday[0].day.condition.icon}`;
+
+      forecastTwoDate.innerHTML=getDayOfWeek(response.forecast.forecastday[1].date_epoch);
+      forecastTwoMin.innerHTML=response.forecast.forecastday[1].day.maxtemp_c;
+      forecastTwoMax.innerHTML=response.forecast.forecastday[1].day.mintemp_c;
+      imgTwo.src="https:"+`${response.forecast.forecastday[1].day.condition.icon}`;
+
+      forecastThreeDate.innerHTML=getDayOfWeek(response.forecast.forecastday[2].date_epoch);
+      forecastThreeMin.innerHTML=response.forecast.forecastday[2].day.maxtemp_c;
+      forecastThreeMax.innerHTML=response.forecast.forecastday[2].day.mintemp_c;
+      imgThree.src="https:"+`${response.forecast.forecastday[2].day.condition.icon}`;
+      betterUnits=false;
+
+    })
+    .catch(function(err) {
+      console.log(":(");
+
+    });
+  }
+  else{
+  fetch('http://api.weatherapi.com/v1/forecast.json?key=68c77b33187c450e90e174348231609&q=47150&days=3&aqi=no&alerts=no')
   .then(function(response) {
     return response.json();
 
@@ -58,3 +99,8 @@ fetch('http://api.weatherapi.com/v1/forecast.json?key=68c77b33187c450e90e1743482
   .catch(function(err) {
     console.log(":(");
   });
+  betterUnits=true;
+}
+
+
+})
