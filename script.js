9e937797f494b8006646420976aee59758ef7fb8
Ivan Profitt <93939178+IvanProfitt@users.noreply.github.com>
2023-09-18 12:03:54 -0400

    Add basic functionality. Displays three-day forecast.

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 const cityName=document.getElementById("cityName");
 const cityTemp=document.getElementById("cityTemp");
+const todayImg=document.getElementById("todayImg");
+const weatherCondition=document.getElementById("weatherCondition");
 
-fetch('http://api.weatherapi.com/v1/current.json?key=68c77b33187c450e90e174348231609&q=47150&aqi=no')
+
+
+const forecastOneDate=document.getElementById("forecastOneDate");
+const forecastOneMin=document.getElementById("forecastOneMin");
+const forecastOneMax=document.getElementById("forecastOneMax");
+const imgOne=document.getElementById("imgOne");
+
+const forecastTwoDate=document.getElementById("forecastTwoDate");
+const forecastTwoMin=document.getElementById("forecastTwoMin");
+const forecastTwoMax=document.getElementById("forecastTwoMax");
+const imgTwo=document.getElementById("imgTwo");
+
+const forecastThreeDate=document.getElementById("forecastThreeDate");
+const forecastThreeMin=document.getElementById("forecastThreeMin");
+const forecastThreeMax=document.getElementById("forecastThreeMax");
+const imgThree=document.getElementById("imgThree");
+
+function getDayOfWeek(epochTime) {
+  const date = new Date(epochTime * 1000); // Convert seconds to milliseconds
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  return daysOfWeek[date.getUTCDay()];
+}
+
+
+fetch('http://api.weatherapi.com/v1/forecast.json?key=68c77b33187c450e90e174348231609&q=47150&days=3&aqi=no&alerts=no')
   .then(function(response) {
     return response.json();
 
@@ -9,6 +35,24 @@ fetch('http://api.weatherapi.com/v1/current.json?key=68c77b33187c450e90e17434823
   .then(function(response){
     cityTemp.innerHTML=response.current.temp_f;
     cityName.innerHTML=response.location.name;
+    weatherCondition.innerHTML=response.current.condition.text;
+    todayImg.src="https:"+`${response.current.condition.icon}`;
+
+
+    forecastOneDate.innerHTML=getDayOfWeek(response.forecast.forecastday[0].date_epoch);
+    forecastOneMin.innerHTML=response.forecast.forecastday[0].day.maxtemp_f;
+    forecastOneMax.innerHTML=response.forecast.forecastday[0].day.mintemp_f;
+    imgOne.src="https:"+`${response.forecast.forecastday[0].day.condition.icon}`;
+
+    forecastTwoDate.innerHTML=getDayOfWeek(response.forecast.forecastday[1].date_epoch);
+    forecastTwoMin.innerHTML=response.forecast.forecastday[1].day.maxtemp_f;
+    forecastTwoMax.innerHTML=response.forecast.forecastday[1].day.mintemp_f;
+    imgTwo.src="https:"+`${response.forecast.forecastday[1].day.condition.icon}`;
+
+    forecastThreeDate.innerHTML=getDayOfWeek(response.forecast.forecastday[2].date_epoch);
+    forecastThreeMin.innerHTML=response.forecast.forecastday[2].day.maxtemp_f;
+    forecastThreeMax.innerHTML=response.forecast.forecastday[2].day.mintemp_f;
+    imgThree.src="https:"+`${response.forecast.forecastday[2].day.condition.icon}`;
 
   })
   .catch(function(err) {
